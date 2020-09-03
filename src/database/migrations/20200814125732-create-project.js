7aedd7276feb015a3fd5b1606f53d007f9f47a5a
chingsley <eneja.kc@gmail.com>
2020-09-03 20:56:47 +0100

    setup for deployment

--- src/database/migrations/20200814125732-create-project.js
+++ src/database/migrations/20200814125732-create-project.js
@@ -1,54 +1,60 @@
 'use strict';
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    return queryInterface.createTable('Projects', {
-      id: {
-        allowNull: false,
-        autoIncrement: true,
-        primaryKey: true,
-        type: Sequelize.INTEGER,
-      },
-      name: {
-        type: Sequelize.STRING,
-        allowNull: false,
-      },
-      githubLink: {
-        type: Sequelize.STRING,
-      },
-      description: {
-        type: Sequelize.TEXT,
-      },
-      stack: {
-        type: Sequelize.STRING,
-        allowNull: false,
-      },
-      technologies: {
-        type: Sequelize.TEXT,
-      },
-      demoLink: {
-        type: Sequelize.TEXT,
-      },
-      userId: {
-        type: Sequelize.INTEGER,
-        allowNull: false,
-        references: {
-          model: 'Users',
-          key: 'id',
+    return queryInterface.createTable(
+      'Projects',
+      {
+        id: {
+          allowNull: false,
+          autoIncrement: true,
+          primaryKey: true,
+          type: Sequelize.INTEGER,
         },
-        onUpdate: 'CASCADE',
-        onDelete: 'CASCADE',
+        name: {
+          type: Sequelize.STRING,
+          allowNull: false,
+        },
+        githubLink: {
+          type: Sequelize.STRING,
+        },
+        description: {
+          type: Sequelize.TEXT,
+        },
+        stack: {
+          type: Sequelize.STRING,
+          allowNull: false,
+        },
+        technologies: {
+          type: Sequelize.TEXT,
+        },
+        demoLink: {
+          type: Sequelize.TEXT,
+        },
+        userId: {
+          type: Sequelize.INTEGER,
+          allowNull: false,
+          references: {
+            model: 'Users',
+            key: 'id',
+          },
+          onUpdate: 'CASCADE',
+          onDelete: 'CASCADE',
+        },
+        createdAt: {
+          allowNull: false,
+          type: Sequelize.DATE,
+          defaultValue: new Date(),
+        },
+        updatedAt: {
+          allowNull: false,
+          type: Sequelize.DATE,
+          defaultValue: new Date(),
+        },
       },
-      createdAt: {
-        allowNull: false,
-        type: Sequelize.DATE,
-        defaultValue: new Date(),
-      },
-      updatedAt: {
-        allowNull: false,
-        type: Sequelize.DATE,
-        defaultValue: new Date(),
-      },
-    });
+      {
+        freezeTableName: true,
+      }
+    );
   },
   // eslint-disable-next-line no-unused-vars
   down: (queryInterface, Sequelize) => {
